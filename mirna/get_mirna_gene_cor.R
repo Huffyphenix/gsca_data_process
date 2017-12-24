d1291393b9e4081dca0cb70d174203d2039282d9
Huffyphenix <949985347@qq.com>
2017-12-24 20:28:00 +0800

    calculate correlation between gene and mirna in each cancers by pearson,p<0.05,cor<-0.3

--- mirna/get_mirna_gene_cor.R
+++ mirna/get_mirna_gene_cor.R
@@ -38,15 +38,16 @@ fn_gene_exp <- function(gene_exp){
   return(gene_exp.p)
 }
 gene_exp %>%
-  dplyr::filter(cancer_types=="CESC") %>%
+  # dplyr::filter(cancer_types=="CESC") %>%
   dplyr::mutate(gene_exp=purrr::map(expr,fn_gene_exp)) %>%
   dplyr::select(-expr) -> gene_exp.gather
 mirna_exp %>%
-  dplyr::filter(cancer_types=="CESC") %>%
+  # dplyr::filter(cancer_types=="CESC") %>%
   dplyr::mutate(mirna_exp=purrr::map(mirna,fn_mirna_exp)) %>%
   dplyr::select(-mirna) -> mirna_exp.gather
 
 fn_get_spm_a <-function(cancer,gene_e) {
+  print(cancer)
   mirna_exp.gather %>%
     dplyr::filter(cancer_types %in% cancer) %>%
     tidyr::unnest() -> mirna_ready
@@ -54,7 +55,7 @@ fn_get_spm_a <-function(cancer,gene_e) {
   gene_e %>%
     tidyr::nest(-symbol) %>%
     dplyr::group_by(symbol) %>%
-    # dplyr::filter(symbol=="A1BG") %>%
+    # dplyr::filter(symbol %in% c("ABI2")) %>%
     dplyr::mutate(spm=purrr::map2(symbol,data,fn_get_spm_b)) %>%
     dplyr::select(-data) %>%
     tidyr::unnest() %>%
@@ -62,13 +63,17 @@ fn_get_spm_a <-function(cancer,gene_e) {
 return(.out)
 }
 fn_get_spm_b <- function(s,data){
-  .out<-data.frame(mirna=1,gene=1,cor=1,p=1) 
+  .out<-data.frame(mirna="test") 
+  # print(s)
   mirna2gene %>%
-    dplyr::filter(symbol %in% s) %>%
-    tidyr::unnest() %>%
-    dplyr::select(mirna) %>%
-    dplyr::inner_join(mirna_ready,by="mirna") ->m_data_ready
+    dplyr::filter(symbol %in% s) ->.symbol_mirna_data
+  if(nrow(.symbol_mirna_data)==1){
+    .symbol_mirna_data %>%
+      tidyr::unnest() %>%
+      dplyr::select(mirna) %>%
+      dplyr::inner_join(mirna_ready,by="mirna") ->m_data_ready
   for(m in m_data_ready$mirna %>% unique()){
+    # print(m)
     m_data_ready %>%
       dplyr::filter(mirna %in% m) %>%
       dplyr::inner_join(data,by="sample") ->tmp
@@ -86,25 +91,29 @@ fn_get_spm_b <- function(s,data){
       # rbind(.out,tmp.out) ->.out
     }
   }
+  }
+  .out$mirna <- .out$mirna %>% as.character()
   return(.out[-1,])
 }
 
 cl<- 10
 cluster <- multidplyr::create_cluster(cl)
 gene_exp.gather %>%
-  # multidplyr::partition(cluster = cluster) %>%
-  # multidplyr::cluster_library("magrittr") %>%
-  # # multidplyr::cluster_assign_value("fun_barcode", fun_barcode)  %>%
-  # multidplyr::cluster_assign_value("fn_get_spm_b", fn_get_spm_b)  %>%
-  # multidplyr::cluster_assign_value("fn_get_spm_a", fn_get_spm_a)  %>%
-  # multidplyr::cluster_assign_value("mirna2gene", mirna2gene)  %>%
-  # multidplyr::cluster_assign_value("mirna_exp.gather",mirna_exp.gather)  %>%
-  # multidplyr::cluster_assign_value("mirna_ready",mirna_ready)  %>%
+  multidplyr::partition(cluster = cluster) %>%
+  multidplyr::cluster_library("magrittr") %>%
+  multidplyr::cluster_assign_value("fn_get_spm_b", fn_get_spm_b)  %>%
+  multidplyr::cluster_assign_value("fn_get_spm_a", fn_get_spm_a)  %>%
+  multidplyr::cluster_assign_value("mirna2gene", mirna2gene)  %>%
+  multidplyr::cluster_assign_value("mirna_exp.gather",mirna_exp.gather)  %>%
+  multidplyr::cluster_assign_value("mirna_ready",mirna_ready)  %>%
   dplyr::mutate(mirna_result=purrr::map2(cancer_types,gene_exp,fn_get_spm_a)) %>%
   dplyr::collect() %>%
   dplyr::ungroup() %>%
-  dplyr::select(-PARTITION_ID,-gene_exp) -> pan33_gene_cor_mirna
+  dplyr::select(-PARTITION_ID) %>%
+  dplyr::select(-gene_exp) -> pan33_gene_cor_mirna
 parallel::stopCluster(cluster)
 
-  
+pan33_gene_cor_mirna %>%
+  readr::write_rds(file.path(config$out,"TCGA","mirna","pan33_gene_cor_with_mirna.rds.gz"),compress="gz")
 
+ # save.image(file = "/project/huff/huff/github/gsca_data_process/mirna/.RData")
