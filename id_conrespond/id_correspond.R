505d5b7ec686808df4f776021b40c3a0b7220092
Huffyphenix <949985347@qq.com>
2019-05-20 05:15:26 +0000

    gtex id correspond ncbi and combine with tcga id

--- id_conrespond/id_correspond.R
+++ id_conrespond/id_correspond.R
@@ -12,6 +12,13 @@ TCGA_RNAseq_gene_info <- readr::read_tsv(file.path(basic_path,"TCGA_lungcancer_d
   dplyr::mutate(n=1:20531) %>%
   tidyr::separate(gene_id,c("Symbol","GeneID"),sep="\\|")
 
+GTEx_gene_info <- readr::read_rds(file.path(basic_path,"data/GSCALite", "GTEx", "expression", "gtex_gene_mean_exp.rds.gz")) %>%
+  head(1) %>%
+  dplyr::select(Mean) %>%
+  tidyr::unnest() %>%
+  dplyr::select(symbol) %>%
+  dplyr::rename("Symbol"="symbol")
+
 # id process --------------------------------------------------------------
 
 fn_get_synonyms <- function(.x){
@@ -47,7 +54,26 @@ TCGA_RNAseq_gene_info %>%
   dplyr::inner_join(ncbi_symbol_info_9606.alias,by="GeneID") %>%
   dplyr::rename("TCGA_sym"="Symbol.x","NCBI_sym"="Symbol.y") %>%
   dplyr::select(-n) %>%
-  dplyr::mutate(TCGA_sym = ifelse(TCGA_sym == "?", NCBI_sym,TCGA_sym))-> gene_info.TCGA.NCBI
+  dplyr::mutate(TCGA_sym = ifelse(TCGA_sym == "?", NCBI_sym,TCGA_sym)) -> gene_info.TCGA.NCBI
 
-gene_info.TCGA.NCBI %>%
+# merge ncbi gene info and GTEX gene info ---------------------------------
+GTEx_gene_info %>%
+  dplyr::inner_join(ncbi_symbol_info_9606.alias,by = "Symbol") %>%
+  dplyr::mutate(NCBI_sym = Symbol) %>%
+  dplyr::rename("GTEX_sym" = "Symbol") -> gene_info.TCGA.GTEXsym
+
+GTEx_gene_info %>%
+  dplyr::filter(!Symbol %in% gene_info.TCGA.GTEXsym$GTEX_sym) -> GTEx_gene.alias
+ncbi_symbol_info_9606.alias %>%
+  dplyr::filter(alias %in% GTEx_gene.alias$Symbol) %>%
+  dplyr::rename("NCBI_sym" = "Symbol") %>%
+  dplyr::mutate(GTEX_sym = alias) %>%
+  dplyr::select(GTEX_sym,GeneID, alias, NCBI_sym)-> gene_info.TCGA.GTEXalias
+gene_info.TCGA.GTEXsym %>%
+  rbind(gene_info.TCGA.GTEXalias) -> gene_info.GTEX.TCGA
+
+gene_info.GTEX.TCGA %>%
+  dplyr::full_join(gene_info.TCGA.NCBI,by=c("GeneID","NCBI_sym","alias")) -> gene_info.merge
+
+gene_info.merge %>%
   readr::write_rds(file.path(basic_path,"data/id_corresponding","id_correspond_between_NCBI_TCGA.rds.gz"),compress="gz")
