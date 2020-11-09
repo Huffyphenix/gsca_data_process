af19a08ed89cc03dd97c6561f401a84d0fbb39da
Huffyphenix <949985347@qq.com>
2020-11-09 22:57:08 +0800

    gene expr stage analysis

--- expr/01.gene_expr_stage.R
+++ expr/01.gene_expr_stage.R
@@ -2,7 +2,7 @@
 # clinical stage with expr ------------------------------------------------
 
 library(magrittr)
-
+library(dplyr)
 
 # load data ---------------------------------------------------------------
 
@@ -10,11 +10,31 @@ data_path <- file.path("/home/huff/data/GSCALite/TCGA")
 gsca_path <- file.path("/home/huff/data/GSCA")
 git_path <- "/home/huff/github/gsca_data_process/expr"
 
-stage_info <- readr::read_rds(file.path(data_path,"clinical","pancan34_clinical_stage.rds.gz"))
+clinical <- readr::read_rds(file.path(data_path,"clinical","pancan34_clinical.rds.gz"))
 
 expr <- readr::read_rds(file.path(gsca_path,"expr","pancan33_expr.IdTrans.rds.gz"))
 
 # combine data ------------------------------------------------------------
+fn_stage_process <- function(cancer_types,.clinical){
+  print(cancer_types)
+  stage_index <- grep("stage",colnames(.clinical),value = T)
+  if(length(stage_index)<1){
+    return(tibble::tibble())
+  } else{
+    .clinical %>%
+      dplyr::select(barcode,stage_index) %>%
+      dplyr::rename(stage=stage_index) %>%
+      dplyr::mutate(stage_type = stage_index) %>%
+      dplyr::filter(!is.na(stage))
+  }
+}
+
+clinical %>%
+  dplyr::mutate(stage = purrr::map2(cancer_types,clinical,fn_stage_process)) %>%
+  dplyr::select(-clinical) %>%
+  dplyr::mutate(n=purrr::map(stage,.f=function(.x){nrow(.x)})) %>%
+  tidyr::unnest(n) %>%
+  dplyr::filter(n>0) -> stage_info
 
 expr %>%
   dplyr::inner_join(stage_info,by="cancer_types") -> combine
@@ -32,6 +52,7 @@ fn_expr_process <- function(.expr) {
     dplyr::select(entrez=entrez_id,symbol,barcode,expr) %>%
     dplyr::distinct()
 }
+
 fn_oneway <- function(.x) {
   .x$stage %>% unique() %>% length() -> .n_stage
   if(.n_stage > 2){
@@ -40,42 +61,47 @@ fn_oneway <- function(.x) {
     .res <- broom::tidy(wilcox.test(log2(expr + 1) ~ stage, data = .x))
   }
   .x %>%
-    tidyr::nest(-stage) %>%
+    tidyr::nest(data=c(barcode, expr, l)) %>%
     dplyr::mutate(mean=purrr::map(data,.f=function(.x){mean(.x$expr)})) %>%
+    dplyr::mutate(n=purrr::map(data,.f=function(.x){nrow(.x)})) %>%
     dplyr::select(-data) %>%
-    tidyr::unnest() %>%
-    tidyr::spread(key="stage",value=mean) -> .mean
+    tidyr::unnest(cols=c(mean,n)) %>%
+    dplyr::mutate(mean.n = paste(signif(mean,2),n,sep = "/"),
+                  stage = paste(stage,"(mean/n)")) %>%
+    dplyr::select(-mean,-n) %>%
+    tidyr::spread(key="stage",value="mean.n") -> .mean
   .res %>%
     dplyr::select(method,pval=p.value) %>%
     dplyr::mutate(fdr=p.adjust(pval, method = "fdr")) %>%
     cbind(.mean) 
 }
-fn_test <- function(cancer_types,expr,stage,n) {
-  print(cancer_types)
-  expr %>% 
+fn_test <- function(.cancer_types,.expr,.stage,n) {
+  print(.cancer_types)
+  .expr %>% 
     fn_expr_process() -> .expr_process
-  stage %>%
+  .stage %>%
     dplyr::inner_join(.expr_process,by="barcode") %>%
     dplyr::distinct() %>%
     dplyr::group_by(symbol,entrez,stage) %>%
-    dplyr::mutate(l = n() > 5) -> .combine
+    dplyr::mutate(l = n()) %>%
+    dplyr::ungroup()-> .combine
   
    #filter out stages with less than 5 samples in one of stage.
     .combine %>%
-      dplyr::filter(l=="FALSE") %>%
+      dplyr::filter(l>=5) %>%
       dplyr::select(stage) %>%
-      dplyr::distinct() -> .stage_less5
+      dplyr::distinct() -> .stage_more5
     
-    if(nrow(.stage_less5)>=3){ #filter out cancers with less than 2 stages
+    if(nrow(.stage_more5)<2){ #filter out cancers with less than 2 stages
       return(tibble::tibble())
     } else {
     .combine %>% 
-      dplyr::filter(!stage %in% .stage_less5$stage) %>%
+      dplyr::filter(stage %in% .stage_more5$stage) %>%
       tidyr::drop_na(expr) %>%
-      tidyr::nest(-symbol,-entrez) %>% 
+      tidyr::nest(data = c(barcode, stage, expr, l)) %>% 
       dplyr::mutate(test = purrr::map(data,.f=fn_oneway)) %>%
-        dplyr::select(-data) %>%
-      tidyr::unnest()
+      dplyr::select(-data) %>%
+      tidyr::unnest(cols=c(test))
   }
 }
 
@@ -84,6 +110,6 @@ combine %>%
   dplyr::select(cancer_types,res) -> stage_results
 
 stage_results %>%
-  readr::write_rds(file.path(gsca_path,"expr","gene_exp_with_stage.rds.gz"),compress = "gz")
+  readr::write_rds(file.path(gsca_path,"expr","pan25_expr_with_stage.rds.gz"),compress = "gz")
 
 save.image(file.path(git_path,"rda","gene_exp_with_stage.rda"))
