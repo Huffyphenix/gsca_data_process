f5a94da90122db4536a3bdc696b63043f89886d4
Huffyphenix <949985347@qq.com>
2020-11-26 11:53:47 +0800

    snv immune cor done

--- immune/3.cnv_immune_cor.R
+++ immune/3.cnv_immune_cor.R
@@ -38,56 +38,127 @@ fn_correlation <- function(.immune,.cnv,.cancers){
   
   .combine %>% 
     tidyr::nest(-symbol,-cell_type,-entrez) %>%
+    # head() %>%
     dplyr::mutate(cor = purrr::map(data,.f=fn_spm)) %>%
     dplyr::select(-data) %>%
-    tidyr::unnest(cor) 
+    tidyr::unnest(cor) -> .tmp
+  .tmp %>%
+    readr::write_rds(file.path(data_path,"TIL",paste(.cancers,"cnv_immune_cor.rds.gz",sep=".")),compress = "gz")
+  .tmp
 }
 
 fn_spm <- function(.data){
-  broom::tidy(cor.test(.data$TIL,.data$cnv,method="kendall")) %>%
-    dplyr::mutate(cor=estimate) %>%
+  
+  kendall = broom::tidy(
+    tryCatch(
+      cor.test(.data$TIL,.data$cnv,method="kendall"),
+    error = function(e) {
+      1
+    })
+    )
+  if(ncol(kendall)>1){
+    kendall %>%
+    dplyr::mutate(cor=estimate) %>%   
     dplyr::mutate(fdr= p.adjust(p.value, method = "fdr")) %>%
     dplyr::mutate(logfdr=-log10(fdr)) %>%
     dplyr::mutate(logfdr=ifelse(logfdr>50,50,logfdr)) -> kendall
+  } else {
+    tibble::tibble(estimate=NA, statistic=NA, p.value=NA, method=NA,alternative=NA) -> kendall
+  }
   
-  broom::tidy(cor.test(.data$TIL,.data$cnv,method="pearson")) %>%
-    dplyr::mutate(cor=estimate) %>%
-    dplyr::mutate(fdr= p.adjust(p.value, method = "fdr")) %>%
-    dplyr::mutate(logfdr=-log10(fdr)) %>%
-    dplyr::mutate(logfdr=ifelse(logfdr>50,50,logfdr)) %>%
-    dplyr::select(-parameter,-conf.low,-conf.high)-> pearson
+  pearson = broom::tidy(
+    tryCatch(
+      cor.test(.data$TIL,.data$cnv,method="pearson"),
+      error = function(e) {
+        1
+      })
+  )
+  if(ncol(pearson)>1){
+    pearson %>%
+      dplyr::mutate(cor=estimate) %>%   
+      dplyr::mutate(fdr= p.adjust(p.value, method = "fdr")) %>%
+      dplyr::mutate(logfdr=-log10(fdr)) %>%
+      dplyr::mutate(logfdr=ifelse(logfdr>50,50,logfdr)) %>%
+      dplyr::select(-parameter,-conf.low,-conf.high)-> pearson
+  } else {
+    tibble::tibble(estimate=NA, statistic=NA, p.value=NA, method=NA,alternative=NA) -> pearson
+  }
   
-  broom::tidy(cor.test(.data$TIL,.data$cnv,method="spearman")) %>%
-    dplyr::mutate(cor=estimate) %>%
-    dplyr::mutate(fdr= p.adjust(p.value, method = "fdr")) %>%
-    dplyr::mutate(logfdr=-log10(fdr)) %>%
-    dplyr::mutate(logfdr=ifelse(logfdr>50,50,logfdr)) -> spearman
+  spearman = broom::tidy(
+    tryCatch(
+      cor.test(.data$TIL,.data$cnv,method="spearman"),
+      error = function(e) {
+        1
+      })
+  )
+  if(ncol(spearman)>1){
+    spearman %>%
+      dplyr::mutate(cor=estimate) %>%   
+      dplyr::mutate(fdr= p.adjust(p.value, method = "fdr")) %>%
+      dplyr::mutate(logfdr=-log10(fdr)) %>%
+      dplyr::mutate(logfdr=ifelse(logfdr>50,50,logfdr)) -> spearman
+  } else {
+    tibble::tibble(estimate=NA, statistic=NA, p.value=NA, method=NA,alternative=NA) -> spearman
+  }
   rbind(pearson,kendall) %>%
     rbind(spearman)
 }
 
 
 # Get the results ---------------------------------------------------------
-cluster <- multidplyr::new_cluster(20)
+# cluster <- multidplyr::new_cluster(33)
+# 
+# multidplyr::cluster_library(cluster,"magrittr")
+# multidplyr::cluster_assign(cluster, fn_correlation=fn_correlation)
+# multidplyr::cluster_assign(cluster, fn_spm=fn_spm)
+# multidplyr::cluster_assign(cluster, data_path=data_path)
+# 
+# 
+# combine_data %>%
+#   dplyr::group_by(cancer_types) %>%
+#   multidplyr::partition(cluster = cluster) %>%
+#   dplyr::mutate(cor=purrr::pmap(list(ImmuneCellAI,cnv,cancer_types),.f=fn_correlation)) %>%
+#   dplyr::collect() %>%
+#   dplyr::select(-ImmuneCellAI,-cnv) -> immune_cnv_correlation
+# 
+# 
+# # result output -----------------------------------------------------------
+# 
+# immune_cnv_correlation %>%
+#   readr::write_rds(file.path(data_path,"TIL","pan33_ImmuneCellAI_cor_geneCNV.rds.gz"))
+# 
+# save.image(file = file.path(git_path,"rda","3.cnv_immune_cor.rda"))
+# 
+# parallel::stopCluster(cluster)
+
+# read all cnv immune files -----------------------------------------------
+
+immune_cnv_file_list <- grep("*cnv_immune_cor.rds.gz",dir(file.path(data_path,"TIL")),value = TRUE)
+
+cancer_types_done <- purrr::map(immune_cnv_file_list,.f=function(.x){strsplit(.x,split = "\\.")[[1]][1]}) %>% unlist()
+
+cluster <- multidplyr::new_cluster(1)
 
 multidplyr::cluster_library(cluster,"magrittr")
 multidplyr::cluster_assign(cluster, fn_correlation=fn_correlation)
 multidplyr::cluster_assign(cluster, fn_spm=fn_spm)
+multidplyr::cluster_assign(cluster, data_path=data_path)
 
 
 combine_data %>%
+  dplyr::filter(!cancer_types %in% cancer_types_done) %>%
   dplyr::group_by(cancer_types) %>%
   multidplyr::partition(cluster = cluster) %>%
   dplyr::mutate(cor=purrr::pmap(list(ImmuneCellAI,cnv,cancer_types),.f=fn_correlation)) %>%
   dplyr::collect() %>%
-  dplyr::select(-ImmuneCellAI,-cnv) -> immune_cnv_correlation
+  dplyr::select(-ImmuneCellAI,-cnv) -> immune_cnv_correlation_restcancers
 
 
 # result output -----------------------------------------------------------
 
-immune_cnv_correlation %>%
-  readr::write_rds(file.path(data_path,"TIL","pan33_ImmuneCellAI_cor_geneCNV.rds.gz"))
-
-save.image(file = file.path(git_path,"rda","3.cnv_immune_cor.rda"))
+# immune_cnv_correlation %>%
+#   readr::write_rds(file.path(data_path,"TIL","pan33_ImmuneCellAI_cor_geneCNV.rds.gz"))
+# 
+# save.image(file = file.path(git_path,"rda","3.cnv_immune_cor.rda"))
 
 parallel::stopCluster(cluster)
