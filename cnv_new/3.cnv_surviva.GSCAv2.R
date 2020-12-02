083dddd30e5255574538c26bdf33295fc4888729
Huffyphenix <949985347@qq.com>
2020-12-02 22:42:29 +0800

    cnv suruvivaal

---
+++ cnv_new/3.cnv_surviva.GSCAv2.R
@@ -0,0 +1,62 @@
+
+######################## cnv survival ############################
+
+library(survival)
+library(magrittr)
+
+# path --------------------------------------------------------------------
+
+data_path <- "/home/huff/data/GSCA"
+gsca_v2_path <- file.path("/home/huff/data/GSCA")
+git_path <- "/home/huff/github/gsca_data_process/cnv"
+
+# load data ---------------------------------------------------------------
+
+sample_info <- readr::read_rds(file.path("/home/huff/data/GSCA/sample_info.rds.gz")) %>%
+  dplyr::mutate(barcode16=substr(barcode,1,16)) %>%
+  dplyr::select(barcode16,cancer_types) %>%
+  unique()
+
+sample_info$cancer_types %>% unique() -> cancer_types
+
+survival <- readr::read_rds(file.path(data_path,"clinical","pancan34_clinical_stage_survival_subtype.rds.gz")) %>%
+  dplyr::select(cancer_types,survival)
+
+cnv <- readr::read_rds(file.path(data_path,"cnv","pancan34_cnv_threshold.IdTrans.rds.gz"))
+
+survival %>%
+  dplyr::inner_join(cnv,by="cancer_types") -> combine_cnv_survival
+# functions ---------------------------------------------------------------
+source("/home/huff/github/gsca_data_process/cnv_new/4.cnv_survival.function.R")
+
+# calculation -------------------------------------------------------------
+filelist <- dir("/home/huff/data/GSCA/cnv/cancer_cnv_survival") 
+cancer_done <- c()
+for (file in filelist) {
+  cancer_done <- c(strsplit(file,"_")[[1]][1],cancer_done)
+}
+cluster <- multidplyr::new_cluster(5)
+multidplyr::cluster_library(cluster,"magrittr")
+multidplyr::cluster_assign(cluster, fn_survival_res=fn_survival_res)
+multidplyr::cluster_assign(cluster, survival_group=survival_group)
+multidplyr::cluster_assign(cluster, fn_cox_logp=fn_cox_logp)
+multidplyr::cluster_assign(cluster, fn_survival=fn_survival)
+multidplyr::cluster_assign(cluster, res_path=res_path)
+multidplyr::cluster_assign(cluster, survival=survival)
+multidplyr::cluster_assign(cluster, gsca_v2_path=gsca_v2_path)
+multidplyr::cluster_assign(cluster, cnv_group=cnv_group)
+multidplyr::cluster_assign(cluster, cancer_done=cancer_done)
+
+combine_cnv_survival %>%
+  dplyr::filter(!cancer_types %in% cancer_done) %>%
+  multidplyr::partition(cluster = cluster) %>%
+  dplyr::group_by(cancer_types) %>%
+  dplyr::mutate(survival_res = purrr::pmap(list(cancer_types,survival,cnv),.f=fn_survival_res)) %>%
+  dplyr::collect()-> pan33_cnv_survival
+
+
+pan33_cnv_survival %>%
+  readr::write_rds(file.path(gsca_v2_path,"cnv","pan33_cnv_survival.rds.gz"),compress = "gz")
+
+save.image(file.path(git_path,"3.cnv_survival-GSCAv2.rda"))
+parallel::stopCluster(cluster)
