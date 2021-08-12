103374fee6e503fb8340f2a38cad6946b0c57c28
Huffyphenix <949985347@qq.com>
2021-08-12 22:33:12 +0800

    new survival for expr data

---
+++ expr/02.functions_survival_NEW_20210812.R
@@ -0,0 +1,154 @@
+library(magrittr)
+library(survival)
+library(dplyr)
+
+
+res_path <- file.path("/home/huff/data/GSCA","expr")
+# transform samples barcode -----------------------------------------------
+
+fn_transform_samples <- function(.x) {
+  .x %>% 
+    tidyr::gather(-entrez_id,-symbol,key = 'aliquot', value = 'expr') ->
+    .y
+  
+  .y %>% 
+    dplyr::mutate(tmp = substr(x = aliquot, start = 14, stop = 14)) %>% 
+    dplyr::mutate(type = ifelse(tmp == '0', 'tumor', 'normal')) %>% 
+    dplyr::mutate(barcode = substr(x = aliquot, start = 1, stop = 16)) %>% 
+    dplyr::mutate(sample_name = substr(x = barcode, start = 1, stop = 12)) 
+}
+
+
+# expr group for survival -------------------------------------------------
+
+survival_group <- tibble::tibble(type=c("os","pfs"),
+                                 time=c("os_days","pfs_days"),
+                                 status=c("os_status","pfs_status"))
+
+fn_expr_group <- function(.data,.cutoff){
+  .data %>%
+    dplyr::mutate(group=ifelse(expr>quantile(expr,.cutoff)[[1]],"Higher expr.", "Lower expr."))
+}
+
+fn_cox_logp <- function(.d){
+  .d %>%
+    dplyr::filter(!is.na(group)) %>%
+    dplyr::filter(!is.na(time)) %>%
+    dplyr::filter(!is.na(status)) %>%
+    dplyr::group_by(group) %>%
+    dplyr::mutate(n=dplyr::n()) %>%
+    dplyr::select(group,n) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(n>=2) %>%
+    .$group %>% unique() %>% length() -> len_group
+  if(len_group==2){
+    kmp <- tryCatch(
+      1 - pchisq(survival::survdiff(survival::Surv(time, status) ~ group, data = .d, na.action = na.exclude)$chisq, df = len_group - 1),
+      error = function(e) {1}
+    )
+    
+    cox_categorical <- tryCatch(
+      broom::tidy(survival::coxph(survival::Surv(time, status) ~ group, data = .d, na.action = na.exclude)),
+      error = function(e) {1}
+    )
+    if (!is.numeric(cox_categorical)) {
+      coxp_categorical <- cox_categorical$p.value
+      hr_categorical <- exp(cox_categorical$estimate)
+    } else {
+      coxp_categorical <- NA
+      hr_categorical <- NA
+    }
+    
+    cox_continus <- tryCatch(
+      broom::tidy(survival::coxph(survival::Surv(time, status) ~ expr, data = .d, na.action = na.exclude)),
+      error = function(e) {1}
+    )
+    if (!is.numeric(cox_continus)) {
+      coxp_continus <- cox_continus$p.value
+      hr_continus <- exp(cox_continus$estimate)
+    } else {
+      coxp_continus <- NA
+      hr_continus <- NA
+    }
+    if(is.numeric(hr_categorical)){
+      if(hr_categorical>1){
+        higher_risk_of_death <- "Lower expr."
+      }else if (hr_categorical<1){
+        higher_risk_of_death <- "Higher expr."
+      } else{
+        higher_risk_of_death <- "Unable to judge"
+      }
+    } else{
+      higher_risk_of_death <- "Not applicable"
+    }
+    
+    
+  } else {
+    kmp<-NA
+    coxp_categorical<-NA
+    coxp_continus<-NA
+    hr_continus <- NA
+    hr_categorical<- NA
+    higher_risk_of_death <- NA
+  }
+  tibble::tibble(logrankp=kmp,
+                 coxp_categorical=coxp_categorical,
+                 hr_categorical=hr_categorical,
+                 coxp_continus=coxp_continus,
+                 hr_continus=hr_continus,
+                 higher_risk_of_death=higher_risk_of_death)
+}
+
+fn_survival <- function(.data,.cutoff,sur_type){
+  .data %>%
+    fn_expr_group(.cutoff = .cutoff) -> .data_grouped
+  
+  survival_group %>%
+    dplyr::filter(type==sur_type) -> sur_type_do
+  
+  .data_grouped %>%
+    dplyr::select(sample_name,group,time=sur_type_do$time,status=sur_type_do$status,expr) %>%
+    fn_cox_logp()
+}
+
+fn_survival_res <- function(.cancer_types,.expr,.survival){
+  # print(paste(.cancer_types))
+  .survival %>%
+    dplyr::filter(cancer_types == .cancer_types) %>%
+    tidyr::unnest(cols = c(combine)) ->.survival_f
+  
+  fn_transform_samples(.expr) -> .expr_t
+  
+  .expr_t %>%
+    dplyr::filter(type=="tumor") %>%
+    dplyr::filter(!is.na(expr)) %>%
+    dplyr::inner_join(.survival_f,by="sample_name") %>%
+    dplyr::group_by(entrez_id, symbol) %>%
+    tidyr::nest()-> .combine
+  
+  # os survival ----
+  
+  .combine %>%
+    dplyr::mutate(res = purrr::map(data,fn_survival,.cutoff=0.5,sur_type="os")) %>%
+    dplyr::select(-data) %>%
+    tidyr::unnest() -> os_median
+  
+  # pfs survival -----
+  if (length(grep("pfs",colnames(.survival_f)))>0) {
+    .survival_f %>% dplyr::filter(!is.na(.survival_f)) %>% nrow() -> .n_pfs
+    if(.n_pfs>0){
+      .combine %>%
+        dplyr::mutate(res = purrr::map(data,fn_survival,.cutoff=0.5,sur_type="pfs")) %>%
+        dplyr::select(-data) %>%
+        tidyr::unnest() -> pfs_median
+    }
+  } else {
+    tibble::tibble(logrankp=NA, hr_categorical=NA,coxp_categorical=NA, coxp_continus=NA,hr_continus=NA,higher_risk_of_death=NA,cutoff=0.5,sur_type="pfs") -> pfs_median
+  }
+  
+  os_median %>%
+    rbind(pfs_median) -> tmp
+  tmp %>%
+    readr::write_rds(file.path(res_path,"survival_new20210812",paste(.cancer_types,"survival.exp.rds.gz",sep="_")),compress = "gz")
+  tmp
+}
---
+++ expr/02.gene_expr_survival_NEW_20210812.R
@@ -0,0 +1,71 @@
+
+# survival analysis of gene expression------------------------------------------
+
+library(survival)
+library(magrittr)
+
+# path --------------------------------------------------------------------
+
+data_path <- "/home/huff/data/GSCA"
+gsca_path <- file.path("/home/huff/data/GSCA")
+git_path <- "/home/huff/github/gsca_data_process/expr"
+
+survival_os <- readr::read_rds("/home/huff/project/TCGA_survival/data/Pancan.Merge.clinical-OS-Age-stage.rds.gz") %>%
+  dplyr::rename("os"="clinical_data")
+survival_pfs <- readr::read_rds("/home/huff/project/data/TCGA-survival-time/cell.2018.survival/TCGA_pancan_cancer_cell_survival_time.rds.gz") %>%
+  dplyr::rename("cancer_types"="type","pfs"="data")
+
+survival_os %>%
+  dplyr::inner_join(survival_pfs,by="cancer_types") %>%
+  dplyr::mutate(combine=purrr::map2(os,pfs,.f=function(.x,.y){
+    .y %>%
+      dplyr::rename("barcode"="bcr_patient_barcode","pfs_status"="PFS","pfs_days"="PFS.time") %>%
+      dplyr::select(barcode,pfs_status,pfs_days) %>%
+      dplyr::full_join(.x,by="barcode") %>%
+      dplyr::mutate(os_days=as.numeric(OS), os_status=as.numeric(Status)) %>%
+      dplyr::select(sample_name = barcode,os_days, os_status,pfs_status,pfs_days)
+  })) %>%
+  dplyr::select(cancer_types,combine) -> survival
+
+survival %>% readr::write_rds(file.path(data_path,"clinical","pancan33_survival_NEW.rds.gz"))
+
+expr <- readr::read_rds(file.path(data_path,"expr","pancan33_expr.IdTrans.rds.gz"))
+
+
+gene_symbol <- readr::read_rds('/home/huff/data/GSCA/id/NCBI_id_in_TCGA-final.rds.gz')
+
+# combine data ------------------------------------------------------------
+
+
+# functions ---------------------------------------------------------------
+source("/home/huff/github/gsca_data_process/expr/02.functions_survival_NEW_20210812.R")
+
+
+# calculation -------------------------------------------------------------
+cluster <- multidplyr::new_cluster(5)
+multidplyr::cluster_library(cluster,"magrittr")
+multidplyr::cluster_assign(cluster, fn_survival_res=fn_survival_res)
+multidplyr::cluster_assign(cluster, fn_transform_samples=fn_transform_samples)
+multidplyr::cluster_assign(cluster, survival_group=survival_group)
+multidplyr::cluster_assign(cluster, fn_expr_group=fn_expr_group)
+multidplyr::cluster_assign(cluster, fn_cox_logp=fn_cox_logp)
+multidplyr::cluster_assign(cluster, fn_survival=fn_survival)
+multidplyr::cluster_assign(cluster, res_path=res_path)
+multidplyr::cluster_assign(cluster, gene_symbol=gene_symbol)
+multidplyr::cluster_assign(cluster, survival=survival)
+
+
+
+expr %>%
+  dplyr::group_by(cancer_types) %>%
+  multidplyr::partition(cluster = cluster) %>%
+  dplyr::mutate(survival_res = purrr::pmap(list(cancer_types,expr),.f=fn_survival_res,.survival=survival)) %>%
+  dplyr::collect() %>%
+  dplyr::select(-data)-> pan33_expr_survival
+
+pan33_expr_survival %>%
+  tidyr::nest(-cancer_types) %>%
+  readr::write_rds(file.path(gsca_path,"expr","pan33_expr_survival_supplement.rds.gz"),compress = "gz")
+
+save.image(file.path(git_path,"02.gene_expr_survival.rda"))
+parallel::stopCluster(cluster)
