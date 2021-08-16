bc34d9ac1129307cac0c52285077d73cacc14160
Huffyphenix <949985347@qq.com>
2021-08-16 14:06:16 +0800

    new stage calculation base on firehose stage data

--- expr/01.gene_expr_stage.R
+++ expr/01.gene_expr_stage.R
@@ -3,6 +3,7 @@
 
 library(magrittr)
 library(dplyr)
+library(doParallel)
 
 # load data ---------------------------------------------------------------
 
@@ -10,28 +11,42 @@ data_path <- file.path("/home/huff/data/GSCALite/TCGA")
 gsca_path <- file.path("/home/huff/data/GSCA")
 git_path <- "/home/huff/github/gsca_data_process/expr"
 
-clinical <- readr::read_rds(file.path(data_path,"clinical","pancan34_clinical.rds.gz"))
+stage <- readr::read_rds(file.path(gsca_path,"clinical","pancan33_survival_age_stage_NEW.rds.gz")) %>%
+  dplyr::mutate(stage=purrr::map(combine,.f=function(.x){
+    .x %>%
+      dplyr::select(barcode,Stage)
+  })) %>%
+  dplyr::select(-combine)
 
 expr <- readr::read_rds(file.path(gsca_path,"expr","pancan33_expr.IdTrans.rds.gz"))
 
 # combine data ------------------------------------------------------------
+stages_included <- tibble::tibble(stage=c("Stage I","Stage I","Stage I",
+                                               "Stage II","Stage II","Stage II","Stage II",
+                                               "Stage III", "Stage III", "Stage III", "Stage III",
+                                               "Stage IV","Stage IV","Stage IV","Stage IV"),
+                                  stage_raw=c("stage i","stage ia","stage ib",
+                                          "stage ii","stage iia","stage iib","stage iic",
+                                          "stage iii","stage iiia","stage iiib","stage iiic",
+                                          "stage iv","stage iva","stage ivb","stage ivc"))
 fn_stage_process <- function(cancer_types,.clinical){
   print(cancer_types)
-  stage_index <- grep("stage",colnames(.clinical),value = T)
+  stage_index <- grep("Stage",colnames(.clinical),value = T)
   if(length(stage_index)<1){
     return(tibble::tibble())
   } else{
     .clinical %>%
-      dplyr::select(barcode,stage_index) %>%
-      dplyr::rename(stage=stage_index) %>%
+      dplyr::filter(!is.na(Stage)) %>%
+      dplyr::rename(stage_raw=Stage) %>%
+      dplyr::inner_join(stages_included,by="stage_raw") %>%
+      dplyr::select(-stage_raw) %>%
       dplyr::mutate(stage_type = stage_index) %>%
       dplyr::filter(!is.na(stage))
   }
 }
 
-clinical %>%
-  dplyr::mutate(stage = purrr::map2(cancer_types,clinical,fn_stage_process)) %>%
-  dplyr::select(-clinical) %>%
+stage %>%
+  dplyr::mutate(stage = purrr::map2(cancer_types,stage,fn_stage_process)) %>%
   dplyr::mutate(n=purrr::map(stage,.f=function(.x){nrow(.x)})) %>%
   tidyr::unnest(n) %>%
   dplyr::filter(n>0) -> stage_info
@@ -75,8 +90,13 @@ fn_oneway <- function(.x) {
     # dplyr::mutate(fdr=p.adjust(pval, method = "fdr")) %>%
     cbind(.mean) 
 }
-fn_test <- function(.cancer_types,.expr,.stage,n) {
+fn_test <- function(.cancer_types) {
   print(.cancer_types)
+  combine %>%
+    dplyr::filter(cancer_types == .cancer_types) -> .tmp
+  .tmp$expr[[1]]-> .expr
+  .tmp$stage[[1]]-> .stage
+  .tmp$n[[1]]-> n
   .expr %>% 
     fn_expr_process() -> .expr_process
   .stage %>%
@@ -101,17 +121,52 @@ fn_test <- function(.cancer_types,.expr,.stage,n) {
       tidyr::nest(data = c(barcode, stage, expr, l)) %>% 
       dplyr::mutate(test = purrr::map(data,.f=fn_oneway)) %>%
       dplyr::select(-data) %>%
-      tidyr::unnest(cols=c(test)) -> .tmp
+      tidyr::unnest(cols=c(test)) %>%
+        dplyr::mutate(cancer_types=.cancer_types)-> .tmp
       .tmp %>%
         dplyr::mutate(fdr=p.adjust(.tmp$pval, method = "fdr"))
   }
 }
 
-combine %>%
-  dplyr::mutate(res = purrr::pmap(list(cancer_types,expr,stage,n),.f=fn_test)) %>%
-  dplyr::select(cancer_types,res) -> stage_results
+fn_parallel_start <- function(n_cores = 5) {
+  n_detected_cores <- parallel::detectCores()
+  global_cluster <<- parallel::makeForkCluster(nnodes = n_cores)
+  doParallel::registerDoParallel(cl = global_cluster)
+}
+fn_parallel_stop <- function() {
+  parallel::stopCluster(cl = global_cluster)
+  foreach::registerDoSEQ()
+}
 
-stage_results %>%
-  readr::write_rds(file.path(gsca_path,"expr","pan25_expr_with_stage.rds.gz"),compress = "gz")
+# calculation ---------------------------------------------------------------------
+fn_parallel_start(n_cores = 5)
+stage_results <- foreach(i = combine$cancer_types, .packages = c('magrittr')) %dopar% {
+  fn_test(i)
+}
+fn_parallel_stop()
 
-save.image(file.path(git_path,"rda","gene_exp_with_stage.rda"))
+# res arrangement ---------------------------------------------------------
+
+for (i in 1:length(stage_results)) {
+  if(i==1){
+    stage_results[[i]] %>%
+      tidyr::nest(-cancer_types)-> stage_results.arrange
+  }else{
+    if(nrow(stage_results[[i]])>0){
+      stage_results[[i]]  %>%
+        tidyr::nest(-cancer_types) %>%
+        rbind(stage_results.arrange)-> stage_results.arrange
+    }else{
+      stage_results.arrange -> stage_results.arrange
+    }
+    
+  }
+}
+# combine %>%
+  # dplyr::mutate(res = purrr::pmap(list(cancer_types,expr,stage,n),.f=fn_test)) %>%
+  # dplyr::select(cancer_types,res) -> stage_results
+
+stage_results.arrange %>%
+  readr::write_rds(file.path(gsca_path,"expr","pan21_expr_with_stage-210813.rds.gz"),compress = "gz")
+
+save.image(file.path(git_path,"rda","gene_exp_with_stage-210813.rda"))
