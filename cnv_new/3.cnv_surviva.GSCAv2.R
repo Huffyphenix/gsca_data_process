bcd4434b68a40f7131e2bc957adf26f049abbc52
Huffyphenix <949985347@qq.com>
2021-08-16 14:03:56 +0800

    collect all res in the end

--- cnv_new/3.cnv_surviva.GSCAv2.R
+++ cnv_new/3.cnv_surviva.GSCAv2.R
@@ -39,7 +39,7 @@ cancer_done <- c()
 for (file in filelist) {
   cancer_done <- c(strsplit(file,"_")[[1]][1],cancer_done)
 }
-cluster <- multidplyr::new_cluster(5)
+cluster <- multidplyr::new_cluster(4)
 multidplyr::cluster_library(cluster,"magrittr")
 multidplyr::cluster_assign(cluster, fn_survival_res=fn_survival_res)
 multidplyr::cluster_assign(cluster, survival_group=survival_group)
@@ -55,12 +55,27 @@ combine_cnv_survival %>%
   dplyr::filter(!cancer_types %in% cancer_done) %>%
   multidplyr::partition(cluster = cluster) %>%
   dplyr::group_by(cancer_types) %>%
-  dplyr::mutate(survival_res = purrr::pmap(list(cancer_types,survival,cnv),.f=fn_survival_res)) %>%
-  dplyr::collect()-> pan33_cnv_survival
+  dplyr::mutate(survival_res = purrr::pmap(list(cancer_types,survival,cnv),.f=fn_survival_res)) %>%  
+  dplyr::collect() -> pan33_cnv_survival
+#
 
+tibble::tibble(done = list.files(file.path(res_path,"cancer_cnv_survival_210813"))) %>%
+  dplyr::group_by(done) %>%
+  dplyr::mutate(cancer_types = strsplit(done,"_")[[1]][1]) %>%
+  dplyr::ungroup() -> done_cancers
 
-pan33_cnv_survival %>%
-  readr::write_rds(file.path(gsca_v2_path,"cnv","pan33_cnv_survival.rds.gz"),compress = "gz")
+cnv_survival<-tibble::tibble()
+for (file in done_cancers$done) {
+  done_cancers %>%
+    dplyr::filter(done==file) -> tmp
+  tmp_res <- readr::read_rds(file.path(res_path,"cancer_cnv_survival_210813",file)) %>%
+    dplyr::mutate(cancer_types=tmp$cancer_types) %>%
+    tidyr::nest(-cancer_types)
+  cnv_survival<- rbind(cnv_survival,tmp_res)
+}
+
+cnv_survival %>%
+  readr::write_rds(file.path(res_path,"pan33_cnv_survival_NEW210813.rds.gz"))
 
 save.image(file.path(git_path,"3.cnv_survival-GSCAv2.rda"))
 parallel::stopCluster(cluster)
