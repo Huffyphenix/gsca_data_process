b3c90e337aeb2b0532931b156bb5b941f1ee36f6
Huffyphenix <949985347@qq.com>
2021-09-14 15:39:58 +0800

    expr DSS-DFI_survival

---
+++ expr/05.functions_exp_DSS-DFI_survival_210914.R
@@ -0,0 +1,172 @@
+library(magrittr)
+library(survival)
+library(dplyr)
+
+
+res_path <- file.path("/home/huff/data/GSCA","expr","DSS-DFI_survival_210914")
+# transform samples barcode -----------------------------------------------
+
+fn_transform_samples <- function(.x) {
+  .x %>% 
+    tidyr::gather(-entrez_id,-symbol,key = 'aliquot', value = 'expr') ->
+    .y
+  
+  .y %>% 
+    dplyr::mutate(tmp = substr(x = aliquot, start = 14, stop = 14)) %>% 
+    dplyr::mutate(type = ifelse(tmp == '0', 'tumor', 'normal')) %>% 
+    dplyr::mutate(barcode = substr(x = aliquot, start = 1, stop = 16)) %>% 
+    dplyr::mutate(sample_name = substr(x = barcode, start = 1, stop = 12)) 
+}
+
+
+# expr group for survival -------------------------------------------------
+
+survival_group <- tibble::tibble(type=c("dss","dfi"),
+                                 time=c("dss_days","dfi_days"),
+                                 status=c("dss_status","dfi_status"))
+
+fn_expr_group <- function(.data,.cutoff){
+  .data %>%
+    dplyr::mutate(group=ifelse(expr>quantile(expr,.cutoff)[[1]],"Higher expr.", "Lower expr."))
+}
+
+fn_cox_logp <- function(.d){
+  .d %>%
+    dplyr::filter(!is.na(group)) %>%
+    dplyr::filter(time!="#N/A") %>%
+    dplyr::filter(status %in% c("1","0")) %>%
+    dplyr::mutate(time=as.numeric(time), status=as.numeric(status)) %>%
+    dplyr::group_by(group) %>%
+    dplyr::mutate(n=dplyr::n()) %>%
+    dplyr::select(group,n) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(n>=2) %>%
+    .$group %>% unique() %>% length() -> len_group
+  .d %>%
+    dplyr::filter(!is.na(group)) %>%
+    dplyr::filter(time!="#N/A") %>%
+    dplyr::filter(status %in% c("1","0")) %>%
+    dplyr::mutate(time=as.numeric(time), status=as.numeric(status)) ->.d
+  if(len_group==2){
+    kmp <- tryCatch(
+      1 - pchisq(survival::survdiff(survival::Surv(time, status) ~ group, data = .d, na.action = na.exclude)$chisq, df = len_group - 1),
+      error = function(e) {1}
+    )
+    
+    cox_categorical <- tryCatch(
+      broom::tidy(survival::coxph(survival::Surv(time, status) ~ group, data = .d, na.action = na.exclude)),
+      error = function(e) {1}
+    )
+    if (!is.numeric(cox_categorical)) {
+      coxp_categorical <- cox_categorical$p.value
+      hr_categorical <- exp(cox_categorical$estimate)
+    } else {
+      coxp_categorical <- NA
+      hr_categorical <- NA
+    }
+    
+    cox_continus <- tryCatch(
+      broom::tidy(survival::coxph(survival::Surv(time, status) ~ expr, data = .d, na.action = na.exclude)),
+      error = function(e) {1}
+    )
+    if (!is.numeric(cox_continus)) {
+      coxp_continus <- cox_continus$p.value
+      hr_continus <- exp(cox_continus$estimate)
+    } else {
+      coxp_continus <- NA
+      hr_continus <- NA
+    }
+    if(!is.na(hr_categorical)){
+      if(is.numeric(hr_categorical)){
+        if(hr_categorical>1){
+          higher_risk_of_death <- "Lower expr."
+        }else if (hr_categorical<1){
+          higher_risk_of_death <- "Higher expr."
+        } else{
+          higher_risk_of_death <- "Unable to judge"
+        }
+      } else{
+        higher_risk_of_death <- "Not applicable"
+      }
+    }else{
+      higher_risk_of_death <- "Not applicable"
+    }
+    
+  } else {
+    kmp<-NA
+    coxp_categorical<-NA
+    coxp_continus<-NA
+    hr_continus <- NA
+    hr_categorical<- NA
+    higher_risk_of_death <- NA
+  }
+  tibble::tibble(logrankp=kmp,
+                 coxp_categorical=coxp_categorical,
+                 hr_categorical=hr_categorical,
+                 coxp_continus=coxp_continus,
+                 hr_continus=hr_continus,
+                 higher_risk_of_death=higher_risk_of_death)
+}
+
+fn_survival <- function(.data,.cutoff,sur_type){
+  .data %>%
+    fn_expr_group(.cutoff = .cutoff) -> .data_grouped
+  
+  survival_group %>%
+    dplyr::filter(type==sur_type) -> sur_type_do
+  
+  .data_grouped %>%
+    dplyr::select(sample_name,group,time=sur_type_do$time,status=sur_type_do$status,expr) %>%
+    fn_cox_logp()
+}
+
+fn_survival_res <- function(.cancer_types,.expr,.survival){
+  print(.cancer_types)
+  .survival %>%
+    dplyr::filter(cancer_types == .cancer_types) %>%
+    tidyr::unnest(cols = c(survival )) ->.survival_f
+  
+  fn_transform_samples(.expr) -> .expr_t
+  
+  .expr_t %>%
+    dplyr::filter(type=="tumor") %>%
+    dplyr::filter(!is.na(expr)) %>%
+    dplyr::inner_join(.survival_f,by="sample_name") %>%
+    dplyr::group_by(entrez_id, symbol) %>%
+    tidyr::nest()-> .combine
+  
+  # dss survival ----
+  if (length(grep("dss",colnames(.survival_f)))>0) {
+    .combine %>%
+      dplyr::mutate(res = purrr::map(data,fn_survival,.cutoff=0.5,sur_type="dss")) %>%
+      dplyr::select(-data) %>%
+      tidyr::unnest()%>%
+      dplyr::mutate(sur_type="dss") -> dss_median
+  } else {
+    print("no dss data")
+    tibble::tibble() -> dss_median
+  }
+  
+  
+  # dfi survival -----
+  if (length(grep("dfi",colnames(.survival_f)))>0) {
+    .survival_f %>% dplyr::filter(!is.na(.survival_f)) %>% nrow() -> .n_dfi
+    if(.n_dfi>0){
+      .combine %>%
+        dplyr::mutate(res = purrr::map(data,fn_survival,.cutoff=0.5,sur_type="dfi")) %>%
+        dplyr::select(-data) %>%
+        tidyr::unnest() %>%
+        dplyr::mutate(sur_type="dfi")-> dfi_median
+    }else{
+      tibble::tibble(logrankp=NA, hr_categorical=NA,coxp_categorical=NA, coxp_continus=NA,hr_continus=NA,higher_risk_of_death=NA,cutoff=0.5,sur_type="dfi") -> dfi_median
+    }
+  } else {
+    tibble::tibble(logrankp=NA, hr_categorical=NA,coxp_categorical=NA, coxp_continus=NA,hr_continus=NA,higher_risk_of_death=NA,cutoff=0.5,sur_type="dfi") -> dfi_median
+  }
+  
+  dss_median %>%
+    rbind(dfi_median) -> tmp
+  tmp %>%
+    readr::write_rds(file.path(res_path,paste(.cancer_types,"DSS-DFI_survival.exp.rds.gz",sep="_")),compress = "gz")
+  tmp
+}
